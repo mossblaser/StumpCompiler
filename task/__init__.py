ebcecd86b14c8294bcff09f10e9ca1edaa4aef15
Jonathan Heathcote <mail@jhnet.co.uk>
2010-10-14 00:38:25 +0100

    Added bare-bones abstract Task class

---
+++ task/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+class Task(object):
+	"""
+	A task is a (possibly complex) "action" that can be performed but which when
+	started will always finish at the end. This class is intended to be abstract.
+	"""
+	
+	def __init__(self, subtasks = None):
+		"""
+		Create a task, optionally specifying a list of subtasks.
+		"""
+		self.subtasks = subtasks or []
+	
+	
+	def set_initial_machine_state(self, machine_state):
+		"""
+		Set the initial machine state for this task. This should only be called
+		once.
+		"""
+		self.subtasks[0].set_initial_machine_state(self, machine_state)
+	
+	
+	def get_final_machine_state(self):
+		"""
+		Get the machine state after this task has executed.  For this to complete
+		successfully the initial machine state must have been passed into this
+		task.
+		"""
+		return self.subtasks[-1].get_final_machine_state()
+	
+	
+	def compile(self):
+		"""
+		Return a sequence of instructions which will accomplish this task.  For this
+		to complete successfully the initial machine state must have been passed
+		into this task.
+		"""
+		instrs = []
+		for subtask in self.subtasks:
+			instrs.extend(subtask.compile())
+		return instrs
+	
+	
+	def __len__(self):
+		"""
+		Returns the size (in words of program memory) needed to represent this task.
+		For this to complete successfully the initial machine state must have been
+		passed into this task.
+		"""
+		return sum(len(subtask) for subtask in self.subtasks)
